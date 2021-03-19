cbee6f2789d8bb952787cee5adb313fdaa4602e6
Kam <camiloandresarevaloc@hotmail.com>
2021-03-19 10:46:00 -0500

    DefaultProps, con defaultprops podemos definir el valor por defecto de los props que recibimos en las argumentos del componente PrimeraApp

--- first-app-react-01/counter-app/src/PrimeraApp.js
+++ first-app-react-01/counter-app/src/PrimeraApp.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types'; // importando propTypes para poder definir l
 // los componentes se nombran usando el UperCamelCase
 // functionalComponents FC
 
-const PrimeraApp = ({ saludo }) => {
+const PrimeraApp = ({ saludo, subtitulo }) => { // definimos otro prop (subtitulos) para establecer el contenido por defercto
 
     return (
         <>
             <h1>{saludo}</h1>
             {/* <p>Trataremos de imprimir un objeto en html para ello usamos el JSON.stringify {JSON.stringify(objeto)}</p> */}
-            <p> estamos retornando en un componente en un fragment</p>
+            <p>{subtitulo}</p>
             <a href='https://dev.to/izakntun/todo-app-con-react-ejemplo-basico-16n0'>enlace</a>
         </>
     )
@@ -18,7 +18,9 @@ const PrimeraApp = ({ saludo }) => {
 PrimeraApp.propTypes = { // usando el propTypes definimos el tipo de dato de los  prop que recibimos y si son requeridos
     saludo: PropTypes.string.isRequired // tipo de dato string y isRequerido = obligatorio
 }
-
-
+PrimeraApp.defaultProps = { // usando el defaultProps podemos definir el contenido por defecto de los props 
+    subtitulo: 'soy un subtitulo',
+    saludo: 'soy un saludo por default'
+}
 // usando el <> y wl </> podremos colocar varias lineas de html
 export default PrimeraApp;
