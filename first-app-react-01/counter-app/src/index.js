0007208d8f0e098869330899f78a113beaf80274
Kam <camiloandresarevaloc@hotmail.com>
2021-03-19 11:03:16 -0500

    Pequeño ejercicio para afianzar conocimientos

--- first-app-react-01/counter-app/src/index.js
+++ first-app-react-01/counter-app/src/index.js
@@ -1,6 +1,7 @@
 
 import ReactDOM from 'react-dom';
 import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
 import './index.css';
 
 // const saludoss = <h1>Hola mundo</h1>;// etiqueta a renderizar esto es un ejem- el verdadero lo importamos desde PrimeraAPP
@@ -10,5 +11,5 @@ const divRoot = document.querySelector('#root'); // capturando etiqueta donde re
 
 
 
-ReactDOM.render(<PrimeraApp/>, divRoot); 
+ReactDOM.render(<CounterApp value = {12345}/>, divRoot); 
 
