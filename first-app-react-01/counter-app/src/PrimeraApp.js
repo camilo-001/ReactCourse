0f72cf5d18cbc3c8c24fc2a453a76590de256af2
Kam <camiloandresarevaloc@hotmail.com>
2021-03-19 10:07:27 -0500

    Comunicación entre componentes, maejo de las properties o prop, estás son enviadas desde el componente padre, el cual se encuentra en el index, el cual se denomina padre al primer uso que tenemos del componente cunado lo usamos en el ReactDOM

--- first-app-react-01/counter-app/src/PrimeraApp.js
+++ first-app-react-01/counter-app/src/PrimeraApp.js
@@ -2,23 +2,30 @@
 // los componentes se nombran usando el UperCamelCase
 // functionalComponents FC
 
-const PrimeraApp = () => {
+const PrimeraApp = ({ saludo = 'Hola, soy una prop por defecto si no se envia nada desde el componenten padre en index.js' }) => {
 
-    const saludo = 'Estamos probando como imprimir un const dentro del HTML';
-    const objeto = {
+/* -- COMUNICACIÓN ENTRE COMPONENTES 
+    saludo, es un property que nos puede enviar directamente el componente padre, esta prop podemos
+    inicializarla y podemos usarla dentro del html solo llamandola.
+*/
+    
+   /*  -- IMPRESION DE VARIABLES EN HTML
+    const saludo = 'Estamos probando como imprimir un const dentro del HTML'; 
+
+      const objeto = {
         nombre: 'objeto',
         apellido: 'literal',
         edad: 23
-    }
+    } */
 
     return (
         <>
-            <h1>Hola desde un FC {saludo}</h1>
-            <p>Trataremos de imprimir un objeto en html para ello usamos el JSON.stringify {JSON.stringify(objeto)}</p>
-            <p class="nutaas"> estamos retornando en un componente en un fragment</p>
+            <h1>{saludo}</h1>
+            {/* <p>Trataremos de imprimir un objeto en html para ello usamos el JSON.stringify {JSON.stringify(objeto)}</p> */}
+            <p> estamos retornando en un componente en un fragment</p>
             <a href='https://dev.to/izakntun/todo-app-con-react-ejemplo-basico-16n0'>enlace</a>
         </>
     )
 }
 // usando el <> y wl </> podremos colocar varias lineas de html
-export default PrimeraApp; 
+export default PrimeraApp;
--- first-app-react-01/counter-app/src/index.js
+++ first-app-react-01/counter-app/src/index.js
@@ -10,4 +10,12 @@ const divRoot = document.querySelector('#root'); // capturando etiqueta donde re
 
 /* usamos el ReactDOM.render para renderizar el codigo, ReactDOM tambien nos ofrece poder crear nuestro arbol de componentes lo cual 
 nos permite comunicarnos entre componentes de una manera sencilla */
-ReactDOM.render(<PrimeraApp />, divRoot); 
+
+
+ReactDOM.render(<PrimeraApp saludo ="Hola ahora enviamos una prop desde el componente padre"/>, divRoot); 
+
+/* Comunicación etre componentes Props:
+
+luego de definir el componente padre <PrimeraApp/> podemos enviar también una property o prop, la cual se recivirá
+en el componente hijo, el cual se encuentra en PrimeraApp.js
+*/
